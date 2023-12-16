1f831f76b2ea3a4cee7b07939d87f65c0f1ccc50
Breeze Concept <breezeconcept3@gmail.com>
2023-12-16 22:37:57 +0100

    initial commit

---
+++ Accounts/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import CustomUser
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(max_length=68, min_length=6, write_only=True)
+    password2 = serializers.CharField(max_length=68, min_length=6, write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'first_name', 'last_name', 'password', 'password2']
+
+    def validate(self, attrs):
+        return super().validate(attrs)
+    
+    def create(self, validated_data):
+        return super().create(validated_data)
+
+
+
+
+
+
+
+
